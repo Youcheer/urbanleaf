458b350516df3949499dbb5b2a527ad495fb1980
Youcheer <youcheer@users.noreply.github.com>
2026-05-18 12:11:56 +0530

    Completely remove Google Photos images list from Hero slider component and fallback to gardener illustration successfully

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,11 +6,7 @@ import { useLanguage } from "../context/LanguageContext";
 import { getPlants } from "../lib/db";
 
 const DEFAULT_HERO_IMAGES = [
-  "https://lh3.googleusercontent.com/aida/ADBb0uhlniPhb1ptGJZv0LRyb89rACzkamaW7w9HrGXqOw8hV8nULPtjCm5Xu2T-e_8KVmm3W4Tm0TOsnFowPzgXidmZYP3FXCpvtCdxDALvZkkW6WvX3vYMdNGQUP5EzQafu9upN2YIqWbhmeS_mto_HRJKX9OgTB_yE0MaTU7gLb1029RBwlXEhmxEQ3JOIITLH-Xuvd7JYBkpFtsAtc99cG8rOLB79X8wRJ7NBFZ9tPtBpTAV1aGhBMkGr8PL",
-  "https://lh3.googleusercontent.com/aida-public/AB6AXuBZgqW4-S_YxJ6a7ZZgAAhkiSNKsB8qdNzgs1LyaYgFSOYohXkPDhDA9-tGrbD99mxAjrOgGIrFknx0rkIQpXHKRuQXOdQrObeWm52RJbiXbX7gCjKIt0wtklSlwjzXCsuT-taSWEZ2Txw1rpHwOqsjeUdD4LLuHNik4rVBJIdL7ixZV59tJJR_ZukmraT1HfGszx3_wQgVTcC6hvHUx9JWoi-EfPDfo6BSr4AGz8N4kfxLQGRrYjVnlUw1WeypLlK9Un9TlufbfDhP",
-  "https://lh3.googleusercontent.com/aida-public/AB6AXuA10AMdkwpZK8gAVI7s0X7YJUplwr6kvNF1t-VGg3o3GX3lnSJ-h1hQXxbavsIloIa8FPAnE6iO0eEvNSsXiLizyEfC8kzDNTo4i_4NrZPe7MalF0lmSvXpiUkptKRto0w63Nqs8eHuqX9m9xLRYNwxlxI8f6MqOnRwia0iToRo9loGWljgRELmg6q5lfTEQih7cwZedndqLbR1Ok-QIHZhLA6hM98tvvJ12bIudUvm7l9YXyd4fQ3Wm2Ld3yFOy296IPshsbnzk4hN",
-  "https://lh3.googleusercontent.com/aida-public/AB6AXuDZ9Z1jOyq682XUwchB1kMAnJN99UFqzsrZmIaYEuGpRzMbSGZgM5JTg2Q7du1HjDEJIbKaRINe1Vbqs16VGIxio5DECRJz9hJVV11eCk-GB18SoM1eiXcm1X_xNOi5L_L5Y8YOdUOFlyV7pVzyul3re6PhOzfn_8Rfnu0IBavP6b752IPi3OfOhBNW5RwF78OuCWZx9hJCoTqQ2bSSLLfLRM9fFyUt-AIOAHQeu-77UUZ0vEXetaRbIUmRMROQiMO89I0zm-FxBf99",
-  "https://lh3.googleusercontent.com/aida-public/AB6AXuAGnxJm_S-zdHr0xMfCViIV1D_ad_iGq_HDtbiOwiFNVUUv6DXiGpBAUlpmQYLwDrouFBhwqB-3P1vOuqj214zN0iD-NNTHjcvPvj9iH51gHpC12aYWOTJqFb7nd-M0Jp_RueB-UAX2uYYtu_mWqxfhqtIdYvG-SknDebi0OSL_hMqXM4zzfTwZmZRvKc8yiPAV92vDs6-kkplHcq20OsNtalXiXTSuI3UO1sSoGJ2DMdD2MBViNiwjjQzCUKi0V0FiyOOlhKKb3_J3"
+  "/about_gardener.jpg"
 ];
 
 export const Hero = () => {
@@ -36,8 +32,7 @@ export const Hero = () => {
         if (uniqueImages.length > 0) {
           // Shuffle and take up to 4 images
           const shuffled = uniqueImages.sort(() => 0.5 - Math.random());
-          // Always ensure the beautiful default crimson anthurium is in the list
-          setHeroImages([DEFAULT_HERO_IMAGES[0], ...shuffled.slice(0, 3)]);
+          setHeroImages(shuffled.slice(0, 4));
         } else {
           setHeroImages(DEFAULT_HERO_IMAGES);
         }
